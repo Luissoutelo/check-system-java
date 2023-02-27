e4ee1aa72cafb42c99ed1b304d5b2e79207b3b7f
Luis Soutelo <luissoutelo14@gmail.com>
2023-02-27 21:19:12 +0000

    Piece and Board

--- Chess-Sytem/src/boardgame/Board.java
+++ Chess-Sytem/src/boardgame/Board.java
@@ -9,6 +9,18 @@ public Board(int rows, int columns) {
 	this.columns = columns;
 	pieces=new Piece[rows][columns];
 }
+public int getRows() {
+	return rows;
+}
+public void setRows(int rows) {
+	this.rows = rows;
+}
+public int getColumns() {
+	return columns;
+}
+public void setColumns(int columns) {
+	this.columns = columns;
+}
 
 
 }
